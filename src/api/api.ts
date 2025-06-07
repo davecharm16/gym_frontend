61cfe594ac5e05c43f89f94edbdcab6ea77a78e7
d_francisgonzales <francisgonzales.dev@gmail.com>
2025-06-07 17:56:40 +0800

    feat: added enhancement

---
+++ src/api/api.ts
@@ -0,0 +1,5 @@
+export const base_url = ''
+
+export const endPoint = {
+  login : ''
+}
--- src/api/auth/auth.ts
+++ src/api/auth/auth.ts
@@ -1,15 +1,16 @@
 import axios from 'axios';
 import type { LoginRequest, LoginResponse } from '../../types/auth';
+import { base_url, endPoint } from '../api';
 
 
 const api = axios.create({
-  baseURL: 'https://example.com/api',
+  baseURL: base_url,
 });
 
 export const loginUser = async (
   credentials: LoginRequest
 ): Promise<LoginResponse> => {
-  const { data } = await api.post('/login', credentials);
+  const { data } = await api.post(endPoint.login, credentials);
   return data;
 };
 
