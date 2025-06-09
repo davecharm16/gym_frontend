c2b2411d47abc6ffbb8516398a2e4c1c758fd3d6
allenDG <allenwalterfiestada@gmail.com>
2025-06-09 10:33:27 +0800

    Feature: added folders and bypassed routing for initial component setup

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../store/auth/authStore';
 
 const Login = () => {
+  
   const login = useAuthStore((state) => state.login);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
--- src/store/auth/authStore.ts
+++ src/store/auth/authStore.ts
@@ -2,6 +2,11 @@ import { create } from 'zustand';
 import { loginUser } from '../../api/auth/auth';
 import type { LoginRequest, User} from '../../types/auth';
 
+const mockUser: User = {
+  id: '1',
+  name: 'John Doe',
+  email: 'johndoe@gmail.com'
+}
 
 interface AuthState {
   user: User | null;
@@ -11,11 +16,12 @@ interface AuthState {
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
-  user: null,
-  token: null,
+  user: mockUser,
+  token: 'mock-token',
   login: async (creds) => {
     const { user, token } = await loginUser(creds);
     set({ user, token });
   },
   logout: () => set({ user: null, token: null }),
 }));
+
