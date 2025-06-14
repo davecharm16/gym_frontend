c94fd34f0425a9a785ee696fa249eea071750504
d_francisgonzales <francisgonzales.dev@gmail.com>
2025-06-14 11:44:56 +0800

    fix: auth state

--- src/pages/profile-management/screens/Session.tsx
+++ src/pages/profile-management/screens/Session.tsx
@@ -3,7 +3,6 @@ import ProfileTable from "../components/Table";
 // src/pages/profile-management/screens/Session.tsx
 const Session = () => {
   return <div style={{ padding: 24 }}> 
-      
       <ProfileTable />
     </div>
 };
