585620d48b19d5b9dc15c96b2c8849aaaec90e74
d_francisgonzales <francisgonzales.dev@gmail.com>
2025-06-22 22:29:43 +0800

    feat added date range picker

---
+++ src/pages/payments/components/Dropdown.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { TextField, MenuItem,  } from "@mui/material";
+import type {TextFieldProps} from '@mui/material';
+
+export type DropdownOption = {
+  label: string;
+  value: string;
+};
+
+type DropdownProps = {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+  options: DropdownOption[];
+  width?: number;
+  height?: number;
+} & Omit<TextFieldProps, "select" | "onChange" | "value">;
+
+const Dropdown: React.FC<DropdownProps> = ({
+  label,
+  value,
+  onChange,
+  options,
+  width = 300,
+  height = 50,
+  ...rest
+}) => {
+  return (
+    <TextField
+      select
+      label={label}
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      size="medium"
+      sx={{
+        width,
+        height,
+        fontSize: "18px",
+        "& .MuiInputBase-root": {
+          height,
+          fontSize: "16px",
+        },
+        "& .MuiInputLabel-root": {
+          fontSize: "16px",
+        },
+      }}
+      {...rest}
+    >
+      {options.map((option) => (
+        <MenuItem key={option.value} value={option.value}>
+          {option.label}
+        </MenuItem>
+      ))}
+    </TextField>
+  );
+};
+
+export default Dropdown;
