036760a8ffb8ec3a7ba5d4e08d587f97d4b8e7a0
davecharmbulaquena <davecharm16@gmail.com>
2025-08-09 23:45:07 +0800

    change api url again

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,4 @@
-export const base_url = 'https://gym-backend-d601a5hfv-davecharm16s-projects.vercel.app/api'
+export const base_url = 'https://gym-backend-ashen.vercel.app/api'
 // export const base_url = 'https://gym-node-backend.onrender.com/api'
 
 export const endPoint = {
